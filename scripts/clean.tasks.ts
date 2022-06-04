0e32248f5127ab6e9c9216691e07bab4b421d2ed
Dmitry Tavern <dogger.work@gmail.com>
2022-06-05 02:50:59 +0300

    fix(gulp): fix bug with build directory cleaner
    gulp-clean creates an bug with empty dist after start/build. For prevent it, I replace gulp-clean on custom rmdir function.

--- scripts/clean.tasks.ts
+++ scripts/clean.tasks.ts
@@ -1,10 +1,9 @@
-import * as gulp from 'gulp'
-import * as clean from 'gulp-clean'
 import taskWrap from './helpers/taskWrap'
+import { rmdir } from './helpers/rmdir'
 
 const { APP_BUILD_DIRNAME } = process.env
 
 export default taskWrap('[task]: clean folders', (done: any) => {
-	gulp.src(APP_BUILD_DIRNAME, { allowEmpty: true }).pipe(clean())
-	done()
+	rmdir(APP_BUILD_DIRNAME)
+	return done()
 })
