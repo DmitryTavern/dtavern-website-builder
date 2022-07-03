72615a75fedb8b41a9e67f3d57d487965f7eabe4
Dmitry Tavern <dogger.work@gmail.com>
2022-07-03 17:34:06 +0300

    fix!: remove cache components config for gulp runtime
    When you start gulp scripts, in node process created components
    config cache. But, if create the component with an artisan in another
    terminal, the cache from the first node process will be not updated
    and the component will be added to the 'none' namespace.
    
    BREAKING CHANGE: in config.ts removed checkConfigIntegrity and not added
    to the components config component will be not added automatic.

--- utilities/components/config.ts
+++ utilities/components/config.ts
@@ -1,10 +1,6 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import * as types from '@types'
-import { getComponentsFormDrive } from './getComponents'
-import { registerComponent } from './registerComponent'
-import { getComponentInfo } from './getComponentInfo'
-import { __, log } from '../logger'
 import { mkdir } from '../directories'
 
 const { APP_COMPONENTS_DIR, APP_COMPONENTS_CONFIG_FILENAME } = process.env
@@ -14,47 +10,11 @@ const componentsConfigPath = path.join(
 	APP_COMPONENTS_CONFIG_FILENAME
 )
 
-let _cacheData: types.ComponentsConfig | undefined
-
-function loadData(): types.ComponentsConfig {
+export function readConfig(): types.ComponentsConfig {
 	if (!fs.existsSync(componentsConfigPath)) return {}
 	return JSON.parse(
 		fs.readFileSync(componentsConfigPath, { encoding: 'utf-8' })
 	)
-}
-
-function checkConfigIntegrity() {
-	if (!fs.existsSync(APP_COMPONENTS_DIR)) return
-
-	const componentsFormDrive = getComponentsFormDrive()
-	const components: types.Component[] = []
-
-	for (const key in componentsFormDrive) {
-		components.push(
-			...components.filter(
-				(component) => componentsFormDrive[key].indexOf(component) < 0
-			)
-		)
-	}
-
-	for (const component of components) {
-		const { category, name } = getComponentInfo(component)
-
-		registerComponent('none', category, name)
-	}
-
-	if (components.length !== 0) {
-		log(__('LOG_COMPONENTS_CONFIG_INTEGRITY'))
-	}
-}
-
-export function readConfig(): types.ComponentsConfig {
-	if (!_cacheData) {
-		_cacheData = loadData()
-		checkConfigIntegrity()
-	}
-
-	return _cacheData
 }
 
 export function writeConfig(data: types.ComponentsConfig): void {
